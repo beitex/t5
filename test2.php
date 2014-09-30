755df8f649c5522fe5cc074c3cb87c640944b48b
beitex <beitex@gmail.com>
2014-09-30 19:44:39 +0300

    Initial commit

---
+++ test2.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 9/22/14
+ * Time: 8:40 PM
+ */
+
+require 'vendor/autoload.php';
+
+echo "test2";
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+
+$log->addWarning('hernja bolshaja');
+
+
